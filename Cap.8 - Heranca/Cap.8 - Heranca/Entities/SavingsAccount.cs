a53ae538f578b57a86fe0c3f52babccec276bd7e
Henrique Marques <hfvmarques@gmail.com>
2021-06-27 20:32:54 -0400

    Heranca

---
+++ Cap.8 - Heranca/Cap.8 - Heranca/Entities/SavingsAccount.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cap._8___Heranca.Entities
+{
+    class SavingsAccount
+    {
+    }
+}
---
+++ Cap.8 - Heranca/Cap.8 - Heranca/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Cap._8___Heranca.Entities;
+
+namespace Cap._8___Heranca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BusinessAccount account = new(8010, "Bob Brown", 100.0, 500.0);
+
+            Console.WriteLine(account.Balance);
+
+            //account.Balance = 200.0
+        }
+    }
+}
