5a392ee1ca57e6a639880e82e63e319469e019a4
Henrique Marques <hfvmarques@gmail.com>
2021-06-27 21:07:46 -0400

    UpcastingEDowncasting

--- UpcastingDowncasting/UpcastingDowncasting/Program.cs
+++ UpcastingDowncasting/UpcastingDowncasting/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using UpcastingDowncasting.Entities;
 
 namespace UpcastingDowncasting
 {
@@ -6,7 +7,36 @@ namespace UpcastingDowncasting
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Account acc = new(1001, "Alex", 0.0);
+            BusinessAccount bacc = new(1002, "Maria", 0.0, 500.0);
+
+            // UPCASTING
+            Account acc1 = bacc;
+            Account acc2 = new BusinessAccount(1103, "Bob", 0.0, 200.0);
+            Account acc3 = new SavingsAccount(1004, "Anna", 0.0, 0.01);
+
+            // DOWNCASTING
+
+            BusinessAccount acc4 = (BusinessAccount)acc2;
+            acc4.Loan(100.0);
+
+            // BusinessAccount acc5 = (BusinessAccount)acc3; O compilador aceita esse código mas o programa não roda
+
+            if (acc3 is BusinessAccount)
+            {
+                // BusinessAccount acc5 = acc3 as BusinessAccount; Sintaxe alternativa para casting
+                BusinessAccount acc5 = (BusinessAccount)acc3;
+                acc5.Loan(200.0);
+                Console.WriteLine("Loan!");
+            }
+
+            if (acc3 is SavingsAccount)
+            {
+                // SavingsAccount acc5 = acc3 as SavingsAccount; Sintaxe alternativa para casting
+                SavingsAccount acc5 = (SavingsAccount)acc3;
+                acc5.UpdateBalance();
+                Console.WriteLine("Update!");
+            }
         }
     }
 }
