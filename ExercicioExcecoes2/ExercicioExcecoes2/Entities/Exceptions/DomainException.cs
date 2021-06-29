b85e1c1d7a6dcbdcd7ec5bbd4ca5a9738a7eb671
Henrique Marques <hfvmarques@gmail.com>
2021-06-29 17:28:53 -0400

    Exercicio Excecoes 2

---
+++ ExercicioExcecoes2/ExercicioExcecoes2/Entities/Exceptions/DomainException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ExercicioExcecoes2.Entities.Exceptions
+{
+    class DomainException : ApplicationException
+    {
+        public DomainException(string message) : base(message)
+        {
+        }
+    }
+}
