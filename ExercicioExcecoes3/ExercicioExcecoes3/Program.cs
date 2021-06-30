2dc5eeb4f5ba31564d87284b1be610ac737d52e2
Henrique Marques <hfvmarques@gmail.com>
2021-06-29 21:15:26 -0400

    Exercicio Excecao 3

---
+++ ExercicioExcecoes3/ExercicioExcecoes3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+using ExercicioExcecoes3.Entities;
+using ExercicioExcecoes3.Entities.Exceptions;
+
+namespace ExercicioExcecoes3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter account data");
+                Console.Write("Number: ");
+                int number = int.Parse(Console.ReadLine());
+                Console.Write("Holder: ");
+                string holder = Console.ReadLine();
+                Console.Write("Initial balance: ");
+                double initialBalance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Withdraw limit: ");
+                double withdrawLimit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.WriteLine();
+
+                Account account = new(number, holder, initialBalance, withdrawLimit);
+
+                Console.Write("Enter amount for withdraw: ");
+                double amount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                account.Withdraw(amount);
+                Console.Write("New balance: " + account.Balance.ToString("F2", CultureInfo.InvariantCulture));
+            }
+            catch (DomainException e)
+            {
+                Console.WriteLine("Withdraw error: "+e.Message);
+            }
+        }
+    }
+}
