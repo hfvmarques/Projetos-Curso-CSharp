17584fafbf096f3ae42bc5834e718e0477bc1134
Henrique Marques <hfvmarques@gmail.com>
2021-07-02 10:10:13 -0400

    StreamWriter

---
+++ StreamWriter_/StreamWriter_/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace StreamWriter_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"D:\Programação\ws-vs2019\StreamWriter_\file1.txt";
+            string targetPath = @"D:\Programação\ws-vs2019\StreamWriter_\file2.txt";
+
+
+            try
+            {
+                string[] lines = File.ReadAllLines(sourcePath);
+
+                using (StreamWriter sw = File.AppendText(targetPath))
+                {
+                    foreach (string line in lines)
+                    {
+                        sw.WriteLine(line.ToUpper());
+                    }
+                }
+            }
+            catch (IOException e) 
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
