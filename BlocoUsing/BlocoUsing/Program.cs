1992f94c4ab5d5c2aaf6a0d24e8c7c8b82772e6f
Henrique Marques <hfvmarques@gmail.com>
2021-07-02 10:03:16 -0400

    Bloco using analitico

---
+++ BlocoUsing/BlocoUsing/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace BlocoUsing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"D:\Programação\ws-vs2019\BlocoUsing\filex.txt";
+
+            try
+            {
+                using (FileStream fs = new FileStream(path, FileMode.Open))
+                {
+                    using (StreamReader sr = new StreamReader(fs))
+                    {
+                        while (!sr.EndOfStream)
+                        {
+                            string line = sr.ReadLine();
+                            Console.WriteLine(line);
+                        }
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
