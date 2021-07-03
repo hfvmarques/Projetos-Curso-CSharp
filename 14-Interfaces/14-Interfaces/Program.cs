12c28e0323f6c63e40bd0554af8a3b6571bb148f
Henrique Marques <hfvmarques@gmail.com>
2021-07-03 10:50:07 -0400

    Interfaces segundo commit

--- 14-Interfaces/14-Interfaces/Program.cs
+++ 14-Interfaces/14-Interfaces/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Globalization;
 using Interfaces.Entities;
+using Interfaces.Services;
 
 namespace Interfaces
 {
@@ -15,8 +16,19 @@ namespace Interfaces
             DateTime start = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
             Console.Write("Return (dd/MM/yyyy hh:mm): ");
             DateTime finish = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+            Console.Write("Enter price per hour: ");
+            double hour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Enter price per day: ");
+            double day = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
             CarRental carRental = new CarRental(start, finish, new Vehicle(model));
+
+            RentalService rentalService = new RentalService(hour, day);
+
+            rentalService.ProcessInvoice(carRental);
+
+            Console.WriteLine("INVOICE:");
+            Console.WriteLine(carRental.Invoice);
         }
     }
 }
