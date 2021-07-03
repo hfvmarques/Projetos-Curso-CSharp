981c4631f2f2b3263019aa61c0ee76c9f0ea4599
Henrique Marques <hfvmarques@gmail.com>
2021-07-03 15:08:09 -0400

    Exercicio Interface

---
+++ ExercicioInterfaces/ExercicioInterfaces/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+using ExercicioInterfaces.Entities;
+using ExercicioInterfaces.Services;
+
+namespace ExercicioInterfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter contract data");
+            Console.Write("Number: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.Write("Date (dd/MM/yyyy): ");
+            DateTime date = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy", CultureInfo.InvariantCulture);
+            Console.Write("Contract value: ");
+            double value = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Enter number of installments: ");
+            int months = int.Parse(Console.ReadLine());
+
+            Contract myContract = new Contract(number, date, value);
+
+            ContractService contractService = new ContractService(new PaypalService());
+            contractService.ProcessContract(myContract, months);
+
+            Console.WriteLine("Installments:");
+            foreach (Installment installment in myContract.Installments)
+            {
+                Console.WriteLine(installment);
+            }
+        }
+    }
+}
