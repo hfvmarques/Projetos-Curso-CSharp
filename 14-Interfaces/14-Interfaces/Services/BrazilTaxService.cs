d7274c7bae9572810146952d39de2de9ab3bbd85
Henrique Marques <hfvmarques@gmail.com>
2021-07-02 20:37:45 -0400

    Interfaces commit inicial

---
+++ 14-Interfaces/14-Interfaces/Services/BrazilTaxService.cs
@@ -0,0 +1,17 @@
+﻿namespace Interfaces.Services
+{
+    class BrazilTaxService
+    {
+        public double Tax(double amount)
+        {
+            if (amount <= 100)
+            {
+                return amount * 0.20;
+            }
+            else
+            {
+                return amount * 0.15;
+            }
+        }
+    }
+}
