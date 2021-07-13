d01fb44e231cf6cb10133afc1304b264a41e46a7
Henrique Marques <hfvmarques@gmail.com>
2021-07-13 11:28:18 -0400

    Func 3

--- Func_/Func_/Program.cs
+++ Func_/Func_/Program.cs
@@ -16,7 +16,7 @@ namespace Func_
             list.Add(new Product("Tablet", 350.50));
             list.Add(new Product("HD Case", 80.90));
 
-            Func<Product, string> func = NameUpper;
+            Func<Product, string> func = p => p.Name.ToUpper();
 
             List<string> result = list.Select(func).ToList();
 
@@ -24,11 +24,6 @@ namespace Func_
             {
                 Console.WriteLine(s);
             }
-        }
-
-        static string NameUpper(Product p)
-        {
-            return p.Name.ToUpper();
         }
     }
 }
