c33dd6b52995e91d035a023fac828707528245ef
Henrique Marques <hfvmarques@gmail.com>
2021-07-13 08:43:34 -0400

    Comparison 1

---
+++ ComparisonT/ComparisonT/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using ComparisonT.Entities;
+
+namespace ComparisonT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+
+            list.Add(new Product("TV", 900.0));
+            list.Add(new Product("Notebook", 1200.0));
+            list.Add(new Product("Tablet", 450.0));
+
+            list.Sort();
+
+            foreach (Product p in list)
+            {
+                Console.WriteLine(p);
+            }
+        }
+    }
+}
