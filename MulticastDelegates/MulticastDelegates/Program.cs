9a59c186027943336dafbf3ed1962de56af36a4d
Henrique Marques <hfvmarques@gmail.com>
2021-07-13 10:37:00 -0400

    Multicast Delegates 1

---
+++ MulticastDelegates/MulticastDelegates/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using MulticastDelegates.Services;
+
+namespace MulticastDelegates
+{
+    delegate void BinaryNumericOperation(double n1, double n2);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 10.0;
+            double b = 12.0;
+
+            BinaryNumericOperation op = CalculationService.ShowSum;
+            op += CalculationService.ShowMax;
+
+            op.Invoke(a, b);
+        }
+    }
+}
