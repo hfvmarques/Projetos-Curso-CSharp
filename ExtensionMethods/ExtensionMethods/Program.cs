56fbe19547db50afc911a5d94c39419e773e30c0
Henrique Marques <hfvmarques@gmail.com>
2021-07-12 21:44:34 -0400

    Extension methods 1

---
+++ ExtensionMethods/ExtensionMethods/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using ExtensionMethods.Extensions;
+
+namespace ExtensionMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = new DateTime(2021, 07, 12, 8, 10, 45);
+            Console.WriteLine(dt.ElapsedTime());
+        }
+    }
+}
