8a383d6695870f9ae4245f5ea2967a6a2c27ff07
Henrique Marques <hfvmarques@gmail.com>
2021-07-12 21:45:32 -0400

    Extension methods 1 - namespace System

--- ExtensionMethods/ExtensionMethods/Extensions/DateTimeExtensions.cs
+++ ExtensionMethods/ExtensionMethods/Extensions/DateTimeExtensions.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Globalization;
+﻿using System.Globalization;
 
-namespace ExtensionMethods.Extensions
+namespace System
 {
     static class DateTimeExtensions
     {
