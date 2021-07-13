6c5964f0f8d30f811f20c67757f1141ce78324f1
Henrique Marques <hfvmarques@gmail.com>
2021-07-13 12:10:50 -0400

    Introducao ao Linq 2

--- LinqIntro/LinqIntro/Program.cs
+++ LinqIntro/LinqIntro/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace LinqIntro
@@ -11,7 +12,7 @@ namespace LinqIntro
             int[] numbers = new int[] { 2, 3, 4, 5 };
 
             //Define the query expression
-            var result = numbers.Where(x => x % 2 == 0).Select(x => x * 10);
+            IEnumerable<int> result = numbers.Where(x => x % 2 == 0).Select(x => x * 10);
 
             //Execute the query
             foreach(int x in result)
