fd2e50e08ce581f79adea4bd705b90654ba7de70
Henrique Marques <hfvmarques@gmail.com>
2021-07-13 19:24:07 -0400

    Linq com Lambda 15

--- LinqComLambda/LinqComLambda/Program.cs
+++ LinqComLambda/LinqComLambda/Program.cs
@@ -69,6 +69,9 @@ namespace LinqComLambda
 
             var r14 = products.Where(p => p.Category.Id == 5).Select(p => p.Price).DefaultIfEmpty(0.0).Average();
             Console.WriteLine("Category 5 average price: "+ r14);
+
+            var r15 = products.Where(p => p.Category.Id == 1).Select(p => p.Price).Aggregate((x, y) => x + y);
+            Console.WriteLine("Category 1 aggregate sum: " + r15);
         }
 
         static void Print<T>(string message, IEnumerable<T> collection)
