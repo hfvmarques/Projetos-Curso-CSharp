1b091407163022028a407eaca69419975c66b9b5
Henrique Marques <hfvmarques@gmail.com>
2021-07-13 09:12:44 -0400

    Introducao a Delegates 1

---
+++ DelegatesIntro/DelegatesIntro/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using DelegatesIntro.Services;
+
+namespace DelegatesIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 10.0;
+            double b = 12.0;
+
+            double result = CalculationService.Sum(a, b);
+            Console.WriteLine(result);
+        }
+    }
+}
