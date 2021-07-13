52d2a2bd9f9246de4440d208a6405d6aaf874ae5
Henrique Marques <hfvmarques@gmail.com>
2021-07-13 08:52:55 -0400

    Comparison 6

--- ComparisonT/ComparisonT/Program.cs
+++ ComparisonT/ComparisonT/Program.cs
@@ -14,9 +14,7 @@ namespace ComparisonT
             list.Add(new Product("Notebook", 1200.0));
             list.Add(new Product("Tablet", 450.0));
 
-            Comparison<Product> comparison = (p1, p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper());
-
-            list.Sort(comparison);
+            list.Sort((p1, p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper()));
 
             foreach (Product p in list)
             {
