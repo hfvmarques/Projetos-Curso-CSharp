fa3dcd8e23491d29e7328e0161019be0323b9bf4
Henrique Marques <hfvmarques@gmail.com>
2021-07-12 21:54:00 -0400

    Extension methods com string

--- ExtensionMethods/ExtensionMethods/Program.cs
+++ ExtensionMethods/ExtensionMethods/Program.cs
@@ -6,8 +6,8 @@ namespace ExtensionMethods
     {
         static void Main(string[] args)
         {
-            DateTime dt = new DateTime(2021, 07, 12, 8, 10, 45);
-            Console.WriteLine(dt.ElapsedTime());
+            string s1 = "Good morning dear students!";
+            Console.WriteLine(s1.Cut(10));
         }
     }
 }
