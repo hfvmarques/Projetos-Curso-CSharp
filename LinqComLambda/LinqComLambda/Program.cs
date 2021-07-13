c61281244334db30d566aa2c72bb058d10857303
Henrique Marques <hfvmarques@gmail.com>
2021-07-13 15:44:58 -0400

    Linq com Lambda 5

--- LinqComLambda/LinqComLambda/Program.cs
+++ LinqComLambda/LinqComLambda/Program.cs
@@ -39,6 +39,9 @@ namespace LinqComLambda
 
             var r4 = products.Where(p => p.Category.Tier == 1).OrderBy(p => p.Price).ThenBy(p => p.Name);
             Print("TIER 1 ORDER BY PRICE THEN BY NAME:", r4);
+
+            var r5 = r4.Skip(2).Take(4);
+            Print("TIER 1 ORDER BY PRICE THEN BY NAME SKIP 2 TAKE 4:", r5);
         }
 
         static void Print<T>(string message, IEnumerable<T> collection)
